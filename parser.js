2740219f9c5ca00d076fd10c0f26ba725231eccc
sutanarief <sutan.abdurrachman@mii.co.id>
2025-05-13 15:45:35 +0700

    add log field

--- parser.js
+++ parser.js
@@ -12,6 +12,8 @@ export const parseMessage = (body) => {
     }
   });
 
+  console.log(fields)
+
   return {
     jenis: isStart ? 'start' : isFinish ? 'finish' : '',
     nama: fields['nama'] || '',
