02f0a2fe5027324feaebbd0fe5ca603b6d349f0e
sutanarief <sutan.abdurrachman@mii.co.id>
2025-05-14 00:42:04 +0700

    update gsheet

--- googleSheet.js
+++ googleSheet.js
@@ -5,6 +5,8 @@ dotenv.config();
 export const sendToGoogleSheet = async (data) => {
     try {
         const response = await axios.post(process.env.GOOGLE_SHEET_URL, data);
+        console.log(response, 'ini response')
+        console.log(response.data, 'ini data response')
         return response.data
     } catch (err) {
         console.error('❌ Gagal kirim ke Google Sheet:', err.message);
