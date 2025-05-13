7fadc13fe329a831ff6b9eb2f2abd7fceb69e4c5
sutanarief <sutan.abdurrachman@mii.co.id>
2025-05-14 00:37:44 +0700

    fixing

--- googleSheet.js
+++ googleSheet.js
@@ -3,7 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const sendToGoogleSheet = async (data) => {
-  const response = await axios.post(process.env.GOOGLE_SHEET_URL, data);
-  console.log(response, 'response dari gsheet')
-  return response
+    try {
+        const response = await axios.post(process.env.GOOGLE_SHEET_URL, data);
+        return response.data
+    } catch (err) {
+        console.error('❌ Gagal kirim ke Google Sheet:', err.message);
+        throw err
+    }
 };
