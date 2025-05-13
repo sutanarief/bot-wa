5c8861fc6b11dbc335614b57e37925fbaa5a2f7d
sutanarief <sutan.abdurrachman@mii.co.id>
2025-05-13 11:21:07 +0700

    fixinf payload

--- parser.js
+++ parser.js
@@ -2,10 +2,11 @@ export const parseMessage = (body) => {
   const lower = body.toLowerCase();
   const isMasuk = lower.includes('start');
   const isPulang = lower.includes('finish');
-
-  const nama = body.match(/Nama:\s*(.*)/i)?.[1]?.split("\n")[0]?.trim() || '';
-  const mobil = body.match(/Mobil:\s*(.*)/i)?.[1]?.split("\n")[0]?.trim() || '';
-  const km = body.match(/KM:\s*(.*)/i)?.[1]?.split("\n")[0]?.trim() || '';
+  const payload = lower.split(' ')
+  console.log(payload)
+  const nama = payload[1]
+  const mobil = payload[2]
+  const km = payload[3]
 
   return {
     jenis: isMasuk ? 'start' : isPulang ? 'finish' : '',
